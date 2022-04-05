bddae84fe58dd20086acfb20afa04d235c8a1d03
nihadsuvalija <nihadsuvalija@gmail.com>
2022-04-05 08:18:23 +0200

    Setup Retrofit Instance

---
+++ app/src/main/java/com/example/moviezone/api/RetrofitInstance.kt
@@ -0,0 +1,15 @@
+package com.example.moviezone.api
+
+import com.example.moviezone.utils.Const
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitInstance {
+    val api: TMDBApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(Const.TMDB_BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(TMDBApi::class.java)
+    }
+}
--- app/src/main/java/com/example/moviezone/api/TMDBApi.kt
+++ app/src/main/java/com/example/moviezone/api/TMDBApi.kt
@@ -2,4 +2,5 @@ package com.example.moviezone.api
 
 interface TMDBApi {
     // TODO: Write the API requests that need to be made to the API
+    // Example of a request: https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher -- THIS IS FOR SEARCH
 }
