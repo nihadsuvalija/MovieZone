2c5ddace41dd423c18637d48fe33aa8e79992eac
nihadsuvalija <nihadsuvalija@gmail.com>
2022-04-11 08:43:20 +0200

    Fixed the HTML in reviews and added createdAt date

--- app/src/main/java/com/example/moviezone/screen/moviedetails/ReviewAdapter.kt
+++ app/src/main/java/com/example/moviezone/screen/moviedetails/ReviewAdapter.kt
@@ -1,7 +1,9 @@
 package com.example.moviezone.screen.moviedetails
 
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
@@ -9,6 +11,7 @@ import com.example.moviezone.R
 import com.example.moviezone.databinding.ReviewBinding
 import com.example.moviezone.model.Review
 import com.example.moviezone.utils.ReviewDiffUtil
+import java.text.SimpleDateFormat
 
 class ReviewAdapter: RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder>() {
 
@@ -25,6 +28,7 @@ class ReviewAdapter: RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder>() {
     class ReviewViewHolder(binding: ReviewBinding): RecyclerView.ViewHolder(binding.root) {
         val author = binding.tvAuthorReview
         val content = binding.tvContentReview
+        var createdAt = binding.tvCreatedAtReview
     }
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -37,7 +41,13 @@ class ReviewAdapter: RecyclerView.Adapter<ReviewAdapter.ReviewViewHolder>() {
 
     override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
         holder.author.text = reviews[position].author
-        holder.content.text = reviews[position].content
+
+        // content has HTML in it we don't want, so we remove the tags and any html related characters
+        holder.content.text = reviews[position].content.replace("\\<[^>]*>","")
+
+        // Example date returned from API: 2020-07-09T14:46:230
+        var date = reviews[position].createdAt.subSequence(0, 10)
+        holder.createdAt.text = date
     }
 
     override fun getItemCount(): Int {
