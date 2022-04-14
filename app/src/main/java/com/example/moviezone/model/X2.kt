5d09a4bc4a7ff9830398fd7149fbe184f09d3577
nihadsuvalija <nihadsuvalija@gmail.com>
2022-04-14 09:53:43 +0200

    Minor code change in X2 model

--- app/src/main/java/com/example/moviezone/model/X2.kt
+++ app/src/main/java/com/example/moviezone/model/X2.kt
@@ -7,7 +7,7 @@ data class X2(
     @SerializedName("image_orientation")
     val imageOrientation: String,
     @SerializedName("medium")
-    val medium: MediumXXX,
+    val medium: Medium,
     @SerializedName("region")
     val region: String
 )
